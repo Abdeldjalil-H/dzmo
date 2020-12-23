9ea0f805c9a7009db75ad6af5b7350ebf9d81ffc
Djalil <djaloulehez3@gmail.com>
2020-12-23 22:33:44 +0100

    Updated STATIC_ROOT

--- dzmo/settings.py
+++ dzmo/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -130,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 
@@ -150,5 +150,5 @@ LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
 #Absolute filesystem path to the directory that will hold user-uploaded files.
-MEDIA_ROOT = 'sumbittions'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
