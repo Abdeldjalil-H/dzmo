e857db48b1f8c9e2dd0a31e3b7705c4346f36718
Djalil <djaloulehez3@gmail.com>
2021-05-28 14:18:54 +0100

    change gmail key

--- dzmo/settings.py
+++ dzmo/settings.py
@@ -144,7 +144,7 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'algerianimoteam@gmail.com'
-EMAIL_HOST_PASSWORD = 'inzpnuffmoefetwc'
+EMAIL_HOST_PASSWORD = 'oldoulhemzkvrsof'#'inzpnuffmoefetwc'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 DEFAULT_FROM_EMAIL = 'algerianimoteam@gmail.com'
