2980c6a22a2f94059e943fb442403e09d47a0bb3
Abdeldjalil <djaloulehez3@gmail.com>
2021-08-08 07:41:41 +0100

    change dependencie of tasks initial form 007

--- tasks/migrations/0001_initial.py
+++ tasks/migrations/0001_initial.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('accounts', '0007_team'),
+        ('accounts', '0008_auto_20210808_0724'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
