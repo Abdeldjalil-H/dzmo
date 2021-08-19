e030c2597f67c4716ce14ed3b0b77e8f27207344
Djalil <djaloulehez3@gmail.com>
2021-08-19 13:59:36 +0100

    inline problem

--- tests/admin.py
+++ tests/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 
-from .models import Test, TestAnswer
+from .models import Test, TestAnswer, TestProblem
 
-admin.site.register(Test)
+class ProblemInline(admin.StackedInline):
+    model = TestProblem
+    extra = 0
+
+class TestAdmin(admin.ModelAdmin):
+    inlines = [ProblemInline]
+admin.site.register(Test, TestAdmin)
 admin.site.register(TestAnswer)
