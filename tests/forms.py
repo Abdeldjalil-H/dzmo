5823a0388f0483e7491d20bb0c63b86a265e385a
Djalil <djaloulehez3@gmail.com>
2021-08-19 14:00:45 +0100

    change Upload form to model from

--- tests/forms.py
+++ tests/forms.py
@@ -1,4 +1,18 @@
-from django import forms 
+from django import forms
+from .models import TestAnswer
 
-class UploadFileForm(forms.Form):
-    file = forms.CharField(label='')
+class UploadFileForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.pb_pk = kwargs.get('pb_pk')
+    
+    def save(self, commit):
+        setattr(self.instance, 'pb_pk', self.pb_pk)
+        super().save(commit=commit)
+        self.instance.set_files_path()
+        self.instance.save()
+    class Meta:
+        model = TestAnswer
+        fields = ['files']
+    
