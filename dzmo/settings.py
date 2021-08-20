942daa36ab43ef1772407582a8cbd25280fe3d75
Djalil <djaloulehez3@gmail.com>
2021-08-20 23:06:27 +0100

    OVERWRITE=True

--- dzmo/settings.py
+++ dzmo/settings.py
@@ -162,6 +162,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 django_heroku.settings(locals())
 
-AWS_S3_FILE_OVERWRITE = False
+AWS_S3_FILE_OVERWRITE = True
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
