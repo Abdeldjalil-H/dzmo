05a95ff31b729379a67fc7f9b3d72b69de161537
Abdeldjalil Hezouat <djaloulehez3@gmail.com>
2021-09-21 21:19:59 +0100

    fix TaskPbView

--- tasks/urls.py
+++ tasks/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     path('list/', TasksList.as_view(), name='tasks-list'),
     path('task<int:task_pk>/', TaskProblemsList.as_view(), name='problems-list'),
     path('task<int:task_pk>/<int:pb_pk>/sub=0/', TaskPbSubmit.as_view(), name='pb-submit'), 
-    path('task<int:task_pk>/<int:pb_pk>', TaskPbView.as_view(), name='pb-view'),
+    path('task<int:task_pk>/<int:pb_pk>/', TaskPbView.as_view(), name='pb-view'),
     path('task<int:task_pk>/<int:pb_pk>/delete/', DeleteSubmission.as_view(), name='delete'),
     #path('correction/list/',name='correction-tasks-list'),
     path('correction/task<int:task_pk>/', TaskSubsList.as_view(), name='task-subs-list'),
