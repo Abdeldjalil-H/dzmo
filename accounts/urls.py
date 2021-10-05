4d309a01f58dd51831d160da85ad6c17c8d200f2
Abdeldjalil Hezouat <djaloulehez3@gmail.com>
2021-10-05 15:00:52 +0100

    fix reset password

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,38 +1,26 @@
-from django.urls import path, reverse_lazy, include
-from django.contrib.auth.views import(
-    LoginView, 
-    LogoutView,
-    PasswordChangeView,
-
-)
-
+from django.urls import path, include
+from django.contrib.auth.views import LogoutView
 from .views import( 
     signup,
+    LoginView,
     Profile,
     PersonalAccount,
     ResetPW,
     ResetPWConfirm,
     ResetDone,
     StudentsRanking,
+    PasswordChangeView,
 )
 
 app_name = 'accounts'
 urlpatterns = [
     path('signup/', signup, name='signup'),
-    path('login/', LoginView.as_view(template_name='accounts/login.html',
-                                    extra_context = {'title':'الدخول إلى الموقع',
-                                                    'btn':'تسجيل الدخول'}), name='login'),
-    path('logout/', LogoutView.as_view(
-                            #success_url = '/'
-                                    ), name = 'logout'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('myaccount/', PersonalAccount.as_view(), name='account'),
     path('verification/', include('verify_email.urls')),
-    path('myaccount/changepw', PasswordChangeView.as_view(
-                            template_name='accounts/edit-profile.html',
-                            extra_context ={'btn':'تغير كلمة المرور'},
-                            success_url = reverse_lazy('accounts:account')
-                                                        ), name = 'password_change'),
-    path('<int:pk>/',Profile.as_view() , name = 'profile'),
+    path('myaccount/changepw', PasswordChangeView.as_view(), name='password_change'),
+    path('<int:pk>/',Profile.as_view() , name='profile'),
     path('students-ranking/', StudentsRanking.as_view(), name='ranking'),
     path('reset/', ResetPW.as_view(), name='reset-pw'),
     path('reset/done/', ResetDone.as_view(), name='pw_reset_done'),
