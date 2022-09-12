f7bdbc7a254b01cb96a187378c15047cecee3c44
Abdeldjalil Hezouat <djaloulehez3@gmail.com>
2022-09-12 12:22:52 +0100

    task level 6

--- tasks/migrations/0003_add_level6.py
+++ tasks/migrations/0003_add_level6.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('tasks', '0001_initial'),
+        ('tasks', '0002_auto_20210815_1116'),
     ]
 
     operations = [
