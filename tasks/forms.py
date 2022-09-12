229566c740593fa9d06d65b86ba646389eef959d
Abdeldjalil Hezouat <djaloulehez3@gmail.com>
2022-09-12 12:12:04 +0100

    task level 6

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,7 +1,5 @@
-from django.forms import ModelForm
 from django import forms
-from .models import TaskComment, TaskProblemSubmission, Task
-from control.forms import AddProblemsForm
+from .models import TaskComment, TaskProblemSubmission, Task, TASKS_LEVELS
 from problems.forms import CommentForm, SubmitForm
 
 class SubmitForm(SubmitForm):
@@ -12,10 +10,8 @@ class CommentForm(CommentForm):
     class Meta(CommentForm.Meta):
         model = TaskComment
 
-LEVELS = [(1,'1'),(2,'2'),(3,'3'),(4,'4'),(5,'5')]
-
 class AddProblemsForm(forms.Form):
   
     statements  = forms.CharField(widget=forms.Textarea(),label ='المسائل')
     task = forms.ModelChoiceField(queryset=Task.objects.all(), label='الواجب')  
-    level       = forms.ChoiceField(choices = LEVELS, label = 'المستوى')
+    level       = forms.ChoiceField(choices = TASKS_LEVELS, label = 'المستوى')
