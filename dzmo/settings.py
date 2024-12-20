ea3c20c752edd6f4cfad449cbec6f5f8ef744ec6
Abdeldjalil Hezouat <djaloulehez3@gmail.com>
2024-12-20 10:02:58 +0000

    use .env file

--- dzmo/settings.py
+++ dzmo/settings.py
@@ -1,35 +1,37 @@
+import datetime
 import os
 from os.path import join
-from json import load
 from pathlib import Path
-import datetime
+
+import environ
 from django.utils import timezone
 
-# TODO: change this to a better way of loading configs
-CONFIGS_FILE_PATH = "dzmo_config.json" if os.getenv("DEV") else "/etc/dzmo_config.json"
+# Initialize environ
+env = environ.Env(
+    DEBUG=(bool, False)  # Set default value of DEBUG to False
+)
 
-with open(CONFIGS_FILE_PATH) as config_file:
-    config = load(config_file)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config.get("SECRET_KEY")
-AWS_ACCESS_KEY_ID = config.get("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = config.get("AWS_SECRET_ACCESS_KEY")
-AWS_STORAGE_BUCKET_NAME = config.get("AWS_STORAGE_BUCKET_NAME")
+SECRET_KEY = env("SECRET_KEY")
 
-AWS_S3_REGION_NAME = "eu-west-3"
-AWS_S3_SIGNATURE_VERSION = "s3v4"
+AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_REGION_NAME = env("AWS_S3_REGION_NAME")
+AWS_S3_SIGNATURE_VERSION = env("AWS_S3_SIGNATURE_VERSION")
+AWS_S3_FILE_OVERWRITE = True
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config.get("DEBUG_VALUE") == "True"
-ALLOWED_HOSTS = ["algerianmo.com", "www.algerianmo.com", "localhost", "127.0.0.1"]
+DEBUG = env("DEBUG")
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
 # Application definition
-
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
@@ -49,7 +51,7 @@ INSTALLED_APPS = [
     "storages",
 ]
 
-AUTH_USER_MODEL = "accounts.User"  # changes the default user model to ours
+AUTH_USER_MODEL = "accounts.User"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -82,38 +84,25 @@ TEMPLATES = [
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": os.getenv("REDIS_URL"),
+        "LOCATION": env("REDIS_URL"),
         "OPTIONS": {"CLIENT_CLASS": "django_redis.client.DefaultClient"},
-        # "KEY_PREFIX": "dzmo"
     }
 }
 
 WSGI_APPLICATION = "dzmo.wsgi.application"
 
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
-"""
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-"""
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": config.get("DB_NAME"),
-        "USER": config.get("DB_USER"),
-        "PASSWORD": config.get("DB_PWD"),
-        "HOST": "localhost",
-        "PORT": "",
+        "NAME": env("DB_NAME"),
+        "USER": env("DB_USER"),
+        "PASSWORD": env("DB_PASSWORD"),
+        "HOST": env("DB_HOST"),
+        "PORT": env("DB_PORT"),
     }
 }
-
-# Password validation
-# https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -130,21 +119,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# Internationalization
-# https://docs.djangoproject.com/en/3.1/topics/i18n/
-
 LANGUAGE_CODE = "ar-DZ"
-
 TIME_ZONE = "Africa/Algiers"
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
 STATIC_ROOT = join(BASE_DIR, "staticfiles")
@@ -152,27 +131,20 @@ STATICFILES_DIRS = [join(BASE_DIR, "static")]
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = config.get("DEFAULT_EMAIL")
-EMAIL_HOST_PASSWORD = config.get("GMAIL_KEY")
+EMAIL_PORT = env("EMAIL_PORT")
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_HOST_USER = env("DEFAULT_EMAIL")
+EMAIL_HOST_PASSWORD = env("EMAIL_KEY")
+DEFAULT_FROM_EMAIL = env("DEFAULT_EMAIL")
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-
-DEFAULT_FROM_EMAIL = config.get("DEFAULT_EMAIL")
 
 VERIFICATION_SUCCESS_TEMPLATE = None
 
 LOGIN_URL = "/accounts/login/"
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/accounts/login/"
-# Absolute filesystem path to the directory that will hold user-uploaded files.
+
 MEDIA_ROOT = join(BASE_DIR, "media")
-
-# django_heroku.settings(locals())
-
-AWS_S3_FILE_OVERWRITE = True
-AWS_DEFAULT_ACL = None
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 LAST_SAFE_SOL_DATE = timezone.make_aware(
     datetime.datetime(2022, 8, 11),
